4ebc5baaf4b845bb49021ec9b281cb51ddff357d
mahek <150836830+mahek2303@users.noreply.github.com>
2024-07-15 11:36:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ function updateDisplay() {
 }
 
 updateDisplay();
+
+// Event listener for button clicks
+const keys = document.querySelector('#calculator-keys');
+keys.addEventListener('click', (event) => {
+    const { target } = event; // Get the clicked element
+    const { value } = target; // Get the value of the clicked element
+    if (!target.matches('button')) {
+        return; // Ignore clicks that are not on buttons
+    }
+
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+        case '%':
+        case '+/-':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
+        default:
+            if (Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
+    }
+
+    updateDisplay();
+});
